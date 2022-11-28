7b4d0527d409bb9aef4afb8f59e93187770ae6ad
Madiyar Kuandyk <2003madi2003@gmail.com>
2022-11-29 01:17:21 +0600

    dll complete

--- double_ll.py
+++ double_ll.py
@@ -10,40 +10,30 @@ class LinkedList:
         self.head = None
         self.tail = None
 
-    def insert(self, data):  # insert at the beginning
-        node = Node(data, self.head, None)
-        if not self.head:
-            self.head = node
-            self.tail = node
-            return
-        self.head = node
-        node.next.prev = node
+    def insert(self, data):
+        self.head = Node(data, self.head, None)
+        if not self.head.next:
+            self.tail = self.head
+        else:
+            self.head.next.prev = self.head
 
-    def append(self, data):  # append at the end
-        node = Node(data, None, self.tail)
-        if not self.head:
-            self.head = node
-        self.tail = node
+    def append(self, data):
+        self.tail = Node(data, None, self.tail)
+        if not self.tail.prev:
+            self.head = self.tail
+        else:
+            self.tail.prev.next = self.tail
 
-    def print_forwards(self):
+    def print(self):
         element = self.head
         while element:
-            print(element.data, end=' => ')
+            print(element.data, end=' -> ')
             element = element.next
         print()
 
     def print_backwards(self):
         element = self.tail
         while element:
-            print(element.data, end=' <= ')
+            print(element.data, end=' <- ')
             element = element.prev
         print()
-
-
-ll = LinkedList()
-ll.insert(5)
-ll.insert(4)
-ll.insert(3)
-ll.append(6)
-ll.append(7)
-ll.print_backwards()
